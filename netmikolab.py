24f31450921dcaf6bc3e688a5371bf4112bb3666
Yutthasil <63070144@it.kmitl.ac.th>
2023-02-13 17:56:57 +0000

    modified netmikolab.py to use Regular expression

--- netmikolab.py
+++ netmikolab.py
@@ -1,3 +1,4 @@
+import re
 from netmiko import ConnectHandler
 
 def get_int_brief(device_params):
@@ -8,10 +9,10 @@ def get_int_brief(device_params):
 def get_route(device_params):
     with ConnectHandler(**device_params) as ssh:
         manage_route = ssh.send_command(f'show ip route vrf management \
-            | include ^C')
+            | include ^C').strip().split('\n')[1:]
         data_route = ssh.send_command(f'show ip route vrf control-data \
-            | include ^C')
-        return manage_route + "\n" + data_route
+            | include ^C').strip().split('\n')[1:]
+        return "\n".join(manage_route + data_route)
 
 def get_int_des(device_params, int_name):
     with ConnectHandler(**device_params) as ssh:
@@ -22,40 +23,33 @@ def get_ip(device_params, int_name):
     data = get_int_brief(device_params)
     result = data.strip().split('\n')
     for line in result[1:]:
-        words = line.split()
-        if words[0][0] == int_name[0] and words[0][-3:] == int_name[1:]:
-            return words[1]
+        int_type, int_num, int_ip = re.search('(\w)\w+(\d+/\d+)\s+(\d+\.\d+\.\d+\.\d+|unassigned).*', line).groups()
+        if int_type == int_name[0] and int_num == int_name[1:]:
+            return int_ip
 
 def get_netmask(device_params, int_name):
     route = get_route(device_params)
     result = route.strip().split('\n')
-    for line in result[1:]:
-        words = line.split()
-        if words[0] == 'C' and words[-1][0] == int_name[0] \
-        and words[-1][-3:] == int_name[1:]:
-            netmask = words[1].split('/')[1]
+    for line in result:
+        netmask, int_type, int_num = re.search('.+/(\d+).+\, (\w)\D+(\d/\d)', line).groups()
+        if int_type == int_name[0] and int_num == int_name[1:]:
             return netmask
+        
 
 def get_des(device_params, int_name):
     int_des = get_int_des(device_params, int_name)
     result = int_des.strip().split('\n')
     for line in result[1:]:
-        words = line.split()
-        if words[0][0] == int_name[0] and words[0][2:] == int_name[1:]:
-            if words[1] != 'admin':
-                return " ".join(words[3:])
-            return " ".join(words[4:])
-
+        int_type, int_num, desc = re.search('(\w)\D(\d/\d).+(N.+|C.+)', line).groups()
+        if int_type == int_name[0] and int_num == int_name[1:]:
+            return desc
 def get_status(device_params, int_name):
     int_des = get_int_des(device_params, int_name)
     result = int_des.strip().split('\n')
     for line in result[1:]:
-        words = line.split()
-        if words[0][0] == int_name[0] and words[0][2:] == int_name[1:]:
-            if words[1] != 'admin':
-                return f'{words[1]}/{words[2]}'
-            return f'{words[1]} {words[2]}/{words[3]}'
-    
+        int_type, int_num, status, proto = re.search('(\w)\D(\d/\d)\s+(\w+ down|\w+)\s+(up|down)', line).groups()
+        if int_type == int_name[0] and int_num == int_name[1:]:
+            return f'{status}/{proto}'
 if __name__ == '__main__':
     device_ip = '172.31.102.4'
     username = 'admin'
